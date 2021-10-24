ebe0e38ce747c2751cb963d4adb81034a6329860
AshleyNikr <arn4006@rit.edu>
2021-10-24 19:02:20 -0400

    Updated

--- study.py
+++ study.py
@@ -1,3 +1,6 @@
+import re
+import csv
+
 def open_file(filename):
     # with open(filename) as file:
     #     print("File open")
@@ -20,13 +23,36 @@ def odd_lines(filename):
 
 def even_letters(filename):
     with open(filename) as file:
+        lines = ""
         for line in file:
             line = line.strip()
-            print(line[::2], end="")
+            lines += line
+        print(lines[::2], end="")
 
+def average_words(filename):
+    with open(filename) as file:
+        avg = 0
+        count = 0
+        for line in file:
+            words = re.findall("[a-zA-Z']+[']?", line)
+            if len(words) > 0:
+                avg += len(words)
+                count += 1
+        return avg / count
+
+def field_count(csv_filename):
+    with open(csv_filename) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        count = 0
+        for row in csv_reader:
+            count += len(row)
+        return count
 
 if __name__ == "__main__":
-    filename = "alice.txt"
+    # filename = "alice.txt"
     # open_file(filename)
     # print(odd_lines(filename))
-    even_letters(filename)
+    # even_letters(filename)
+    # print(average_words(filename))
+    print(field_count("grades_010.csv"))
